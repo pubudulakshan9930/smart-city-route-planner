df582220d6ad4eeb25448daa2dc31eb50877dcfc
P.L.W.C.MOHOTTI <22ug3-0282@sltc.ac.lk>
2025-10-21 20:48:50 +0530

    Remove Graph class implementation

--- src/Graph.java
+++ src/Graph.java
@@ -1,18 +1,1 @@
-import java.util.*;
 
-public class Graph {
-    private Map<String, List<String>> adjacencyList;
-    private Set<String> locations;
-
-    public Graph() {
-        adjacencyList = new HashMap<>();
-        locations = new HashSet<>();
-    }
-
-    public void addLocation(String loc) {
-        if (!locations.contains(loc)) {
-            locations.add(loc);
-            adjacencyList.put(loc, new ArrayList<>());
-        }
-    }
-}
